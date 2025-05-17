1b634f06f8cf82695d1ab1e4bedb1d08b1bd70e5
Raymond Chua <ray.r.chua@gmail.com>
2025-05-17 16:45:55 -0400

    refactor: modify envs import to facilitate environment registration

--- gym_miniworld/__init__.py
+++ gym_miniworld/__init__.py
@@ -1,7 +1,10 @@
 # Import the envs module so that envs register themselves
 
 # Import the envs module so that envs register themselves
-from . import envs
+# from . import envs
 
 # Import wrappers so it's accessible when installing with pip
 #from . import wrappers
+def register():
+    """Call this to register MiniWorld environments with Gym."""
+    import gym_miniworld.envs
