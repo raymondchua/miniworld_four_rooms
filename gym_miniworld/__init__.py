d3fc09ddde4d3578665ccaf34d75b4cc6421dd21
Raymond Chua <ray.r.chua@gmail.com>
2025-05-17 16:26:56 -0400

    refactor: delay envs import to avoid circular dependency issues

--- gym_miniworld/__init__.py
+++ gym_miniworld/__init__.py
@@ -1,7 +1,12 @@
 # Import the envs module so that envs register themselves
 
 # Import the envs module so that envs register themselves
-from . import envs
+# from . import envs
 
 # Import wrappers so it's accessible when installing with pip
 #from . import wrappers
+
+def _register_envs():
+    import gym_miniworld.envs  # delay the import to avoid circular issues
+
+_register_envs()
